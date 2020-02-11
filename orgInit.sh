10e620322d2bb2f21a2bd2b713a42834c795afc9
GuidoOktana <guido@oktana.io>
2020-02-11 15:50:49 -0300

    basic onboarding flow

--- orgInit.sh
+++ orgInit.sh
@@ -1,6 +1,5 @@
 sfdx shane:org:create -f config/project-scratch-def.json -d 14 -s --wait 60 --userprefix admin -o flowbuilder.demo
 sfdx force:source:push
 sfdx force:user:permset:assign -n FlowBuilder
-sfdx force:apex:execute -f scripts/init.cls
 sfdx shane:user:password:set -g User -l User -p salesforce1
 sfdx force:org:open
