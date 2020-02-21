1b7fee867061a188d107594b58206d9fdc931845
GuidoOktana <guido@oktana.io>
2020-02-21 11:09:17 -0300

    Flow changes

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,6 @@
 sfdx shane:org:create -f config/project-scratch-def.json -d 14 -s --wait 60 --userprefix admin -o flowbuilder.demo
+sfdx force:data:record:update -s User -w "Name='User User'" -v "FirstName='Admin' LastName='User'"
 sfdx force:source:push
 sfdx force:user:permset:assign -n FlowBuilder
-sfdx shane:user:password:set -g User -l User -p salesforce1
+sfdx shane:user:password:set -g Admin -l User -p salesforce1
 sfdx force:org:open
