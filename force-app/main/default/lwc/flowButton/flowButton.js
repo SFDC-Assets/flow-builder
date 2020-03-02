3fe3be8b7c3f66611a89973d03691cb34f874f7d
Gabe Sumner <gsumner@gsumner-ltm8.internal.salesforce.com>
2020-03-01 20:18:30 -0800

    Enabled customizable flow button

--- force-app/main/default/lwc/flowButton/flowButton.js
+++ force-app/main/default/lwc/flowButton/flowButton.js
@@ -2,6 +2,7 @@ import { LightningElement, api } from 'lwc';
 import { FlowAttributeChangeEvent, FlowNavigationNextEvent } from 'lightning/flowSupport';
 
 export default class flowButton extends LightningElement {
+    @api label;
     handleClick() {
         console.log('handleClick Next');
         const navigateNextEvent = new FlowNavigationNextEvent();
