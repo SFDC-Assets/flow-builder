db5e923f89f4ab7381abc01d47d4886479ff7b95
Gabe Sumner <gsumner@gsumner-ltm8.internal.salesforce.com>
2020-03-10 15:11:27 -0700

    Using regular org:create

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,5 @@
-sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix admin -o flowbuilder.demo
+# sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix admin -o flowbuilder.demo
+sfdx force:org:create -f config/project-scratch-def.json -d 30 -w 60
 sfdx force:data:record:update -s User -w "Name='User User'" -v "FirstName='Admin' LastName='User'"
 sfdx force:source:push
 sfdx force:user:permset:assign -n FlowBuilder
