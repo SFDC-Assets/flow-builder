fefaa110ca29d03d2cfb404ba82b9b20921017f6
Gabe Sumner <gsumner@gsumner-ltm8.internal.salesforce.com>
2020-03-10 15:23:23 -0700

    Removed name change

--- orgInit.sh
+++ orgInit.sh
@@ -1,8 +1,7 @@
 # sfdx shane:org:create -f config/project-scratch-def.json -d 30 -s --wait 60 --userprefix admin -o flowbuilder.demo
 sfdx force:org:create -f config/project-scratch-def.json -s -d 30 -w 60
-sfdx force:data:record:update -s User -w "Name='User User'" -v "FirstName='Admin' LastName='User'"
 sfdx force:source:push
 sfdx force:user:permset:assign -n FlowBuilder
-sfdx shane:user:password:set -g Admin -l User -p salesforce1
+sfdx shane:user:password:set -g User -l User -p salesforce1
 sfdx force:apex:execute -f scripts/generateAccountOrder.apex
 sfdx force:org:open
