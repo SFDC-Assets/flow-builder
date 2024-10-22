8bb3cf05aeb089cbc04a59e102c0fbb824b42cb7
Sumanth Ghanta <92057458+sghanta-sf@users.noreply.github.com>
2024-10-22 12:03:09 -0700

    Update orgInit.sh with cli updates

--- orgInit.sh
+++ orgInit.sh
@@ -1,6 +1,7 @@
-sfdx force:org:create -f config/project-scratch-def.json -s -d 30 -w 60
-sfdx force:source:push
-sfdx force:user:permset:assign -n FlowBuilder
-sfdx shane:user:password:set -g User -l User -p salesforce1
-sfdx force:apex:execute -f scripts/generateAccountOrder.apex
-sfdx force:org:open
+sf demoutil org create scratch -f config/project-scratch-def.json -d 30 -s -w 60
+
+sf project deploy start
+sf org assign permset -n FlowBuilder
+sf demoutil user password set -p salesforce1 -g User -l User
+sf apex run -f scripts/generateAccountOrder.apex
+sf org open
